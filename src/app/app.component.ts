07df33622fa300c401ed1b3ac83895942f6319d0
Krishna_Reddy <kundhurukrishnareddy@gmail.com>
2024-07-24 21:19:15 +0530

    spartan-ui setup done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrnAccordionContentComponent } from '@spartan-ng/ui-accordion-brain';
+import { HlmAccordionDirective, HlmAccordionItemDirective, HlmAccordionTriggerDirective, HlmAccordionContentDirective, HlmAccordionIconDirective } from '@spartan-ng/ui-accordion-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, HlmButtonDirective,
+    BrnAccordionContentComponent,
+    HlmAccordionDirective,
+    HlmAccordionItemDirective,
+    HlmAccordionTriggerDirective,
+    HlmAccordionContentDirective,
+    HlmAccordionIconDirective,
+    HlmIconComponent,
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-client';
+  title = 'DSA Tracker';
 }
